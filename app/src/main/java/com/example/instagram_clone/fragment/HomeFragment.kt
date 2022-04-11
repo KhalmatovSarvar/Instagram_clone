9dfad0b5b211fd5d322ce12f5ed75514f8f9587a
Developer <Jaxadev>
2022-04-11 15:49:32 +0500

    third

--- app/src/main/java/com/example/instagram_clone/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/instagram_clone/fragment/HomeFragment.kt
@@ -6,11 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.instagram_clone.R
+import com.example.instagram_clone.adapter.HomeAdapter
+import com.example.instagram_clone.model.Post
 import java.lang.RuntimeException
 
 class HomeFragment:BaseFragment() {
     val TAG = HomeFragment::class.java.simpleName
+    lateinit var recyclerView: RecyclerView
     private var listener:HomeFragment.HomeListener? = null
 
     lateinit var iv_photo: ImageView
@@ -46,10 +51,33 @@ class HomeFragment:BaseFragment() {
     }
 
     private fun initViews(view: View) {
+        recyclerView = view.findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = GridLayoutManager(activity,1)
+
+        val iv_camera = view.findViewById<ImageView>(R.id.iv_camera)
+        iv_camera.setOnClickListener {
+            listener!!.scrollToUpload()
+        }
+
+        refreshAdapter(loadPosts())
         iv_photo = view.findViewById(R.id.iv_camera)
         iv_photo.setOnClickListener {
         listener!!.scrollToUpload()
         }
+    }
+
+    private fun refreshAdapter(items: ArrayList<Post>) {
+        val adapter = HomeAdapter(this,items)
+        recyclerView.adapter = adapter
+
+    }
+
+    private fun loadPosts(): ArrayList<Post> {
+        val items = ArrayList<Post>()
+        items.add(Post("https://images.unsplash.com/photo-1626497361649-81cc097e9bfd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8bXVzbGltfGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60"))
+        items.add(Post("https://images.unsplash.com/photo-1537511446984-935f663eb1f4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8bWFufGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60"))
+        items.add(Post("https://images.unsplash.com/photo-1488161628813-04466f872be2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fG1hbnxlbnwwfDF8MHx8&auto=format&fit=crop&w=500&q=60"))
+        return items
     }
 
     /*
